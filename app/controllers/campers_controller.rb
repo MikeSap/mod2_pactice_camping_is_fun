a8681a5fb0ce37e77214a6770ea8c099538c61d9
mike <mikesapienzaaudio@gmail.com>
2020-10-14 22:05:51 -0500

    add new for camper

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -1,5 +1,9 @@
-class CampersController < ApplicationController
   def index
-    @campers = Camper.all
-  end
+    def index
+      @campers = Camper.all
+    end
+  
+    def show
+      @camper = Camper.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get "/authors/new", to: "authors#new", as: "new_author"
-  get "/authors/:id", to: "authors#show", as: "author"
-  post "/authors", to: "authors#create"
-
-  get "/posts/:id", to: "posts#show", as: "post"
-  get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
-  patch "/posts/:id", to: "posts#update"
+  resources :activities, only: [:index, :show]
+  resources :campers, only: [:index, :show]
+  resources :signup, only: [:new, :create, :index, :show]
 end
